2fb7d183ca29933474030a07eb5b2cc1a19de83d
Matheus Coutinho <matheuscoutinho.0420@gmail.com>
2025-05-07 14:32:10 -0300

    register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -20,5 +20,42 @@ module.exports = class AuthController {
 
             return
         }
+
+        //vendo se o usuario ja existe
+        const checkIfUserExists = await User.findOne({ where: { email: email } })
+
+        if (checkIfUserExists) {
+            req.flash('message', 'O email já está em uso!')
+            res.render('auth/register')
+
+            return
+        }
+
+        //criando senha
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try {
+            const createdUser = await User.create(user)
+
+            req.session.userid = createdUser.id
+            req.session.save(() =>{
+                res.redirect('/')
+            })
+           
+            req.flash('message', 'Cadastro realizado com sucesso')
+            
+            res.redirect('/')
+
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 }
