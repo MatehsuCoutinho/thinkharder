8f78550af06d4c517b47650a30f062bccc4edf4e
Matheus Coutinho <matheuscoutinho.0420@gmail.com>
2025-05-09 12:32:43 -0300

    post e form dos pensamentos

--- controllers/toughts.controller.js
+++ controllers/toughts.controller.js
@@ -1,12 +1,35 @@
 const Tought = require('../models/tought.model')
 const User = require('../models/user.model')
 
-module.exports = class ToughtsController{
-    static async showToughts(req, res){
+module.exports = class ToughtsController {
+    static async showToughts(req, res) {
         res.render('toughts/home')
     }
 
-    static async dashboard(req, res){
+    static async dashboard(req, res) {
         res.render('toughts/dashboard')
+    }
+
+    static createTought(req, res) {
+        res.render('toughts/create')
+    }
+
+    static async createToughtSave(req, res) {
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid
+        }
+
+        try {
+            await Tought.create(tought)
+
+            req.flash('message', 'Pensamento criado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
--- routes/toughts.routes.js
+++ routes/toughts.routes.js
@@ -5,6 +5,8 @@ const ToughtController = require('../controllers/toughts.controller')
 const checkAuth = require('../helpers/auth').checkAuth
 
 router.get('/', ToughtController.showToughts)
+router.get('/add', checkAuth, ToughtController.createTought)
+router.post('/add', checkAuth, ToughtController.createToughtSave)
 router.get('/dashboard', checkAuth, ToughtController.dashboard)
 
 module.exports = router
